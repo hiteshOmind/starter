9e257cefa8d7cc3b6a4814439e3a3bae4222db19
hitesh <hitesh.kumar@omindtech.com>
2024-07-05 03:32:45 +0530

    my config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,8 +4,44 @@
 ---@type ChadrcConfig
 local M = {}
 
+M.plugins = {
+    {
+		"shellRaining/hlchunk.nvim",
+			event = { "BufReadPost", "BufReadPre" },
+				config = function()
+					require('hlchunk').setup {
+						chunk = {
+						enable = true,
+						use_treesitter = true,
+						notify = true,
+				priority = 5,
+				style={
+					{ fg = "#FF0000" },
+					{ fg = "#FF7F00" },
+					{ fg = "#FFFF00" },
+					{ fg = "#00FF00" },
+					{ fg = "#0000FF" },
+					{ fg = "#4B0082" },
+					{ fg = "#8F00FF" },
+							},
+					delay=300
+				},
+						indent = {
+						enable = true,
+						},
+						line_num = {
+						enable = true,
+						},
+						blank = {
+						enable = true,
+				},
+			}
+		end
+	}
+}
+
 M.ui = {
-	theme = "onedark",
+	theme = "chadracula",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,12 +7,70 @@ return {
     end,
   },
 
+  {
+    "nmac427/guess-indent.nvim",
+    config = function() 
+      require('guess-indent').setup{
+        auto_cmd = true,  -- Set to false to disable automatic execution
+        override_editorconfig = false, -- Set to true to override settings set by .editorconfig
+        filetype_exclude = {  -- A list of filetypes for which the auto command gets disabled
+          "netrw",
+          "tutor",
+        },
+        buftype_exclude = {  -- A list of buffer types for which the auto command gets disabled
+          "help",
+          "nofile",
+          "terminal",
+          "prompt",
+        },
+      }
+    end
+  },
+
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPost", "BufReadPre"},
+    config = function()
+    require("hlchunk").setup {
+        chunk = {
+          enable = true,
+          notify = true,
+          style = {
+            { fg = "#01ffff" },
+            { fg = "#FF7F00" },
+            { fg = "#FFFF00" },
+            { fg = "#00FF00" },
+            { fg = "#0000FF" },
+            { fg = "#4B0082" },
+            { fg = "#8F00FF" },
+          },
+          chars = {
+            horizontal_line = "─",
+            vertical_line = "│",
+            left_top = "╭",
+            left_bottom = "╰",
+            right_arrow = ">",
+          },
+          delay = 400,
+          duration = 300
+        },
+        indent = {
+          enable = false,
+        },
+        line_num = {
+          enable = false,
+        },
+        blank = {
+          enable = false,
+        },
+      }
+    end,
+  }
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
   --   config = function()
   --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
   --   end,
   -- },
   --
